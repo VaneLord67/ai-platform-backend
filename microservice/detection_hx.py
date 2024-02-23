7437e4a8d6e25444ec812d0c43a225210ff45841
CJR <524429470@qq.com>
2024-02-23 14:56:51 +0800

    fix: mp4

--- microservice/detection_hx.py
+++ microservice/detection_hx.py
@@ -83,21 +83,19 @@ class DetectionService(AIBaseService):
                         break
                     # 对帧进行处理
                     results, input_images = inference(image)
-                    frames = parse_results(results)
+                    rects = parse_results(results)
                     json_items = []
-                    if len(frames) > 0:
-                        this_frame = frames[0]
-                        for rect in this_frame:
-                            xmin, ymin, w, h, label, score = rect
-                            json_item = {
-                                'xmin': xmin,
-                                'ymin': ymin,
-                                'w': w,
-                                'h': h,
-                                'label': label,
-                                'score': score,
-                            }
-                            json_items.append(json_item)
+                    for rect in rects:
+                        xmin, ymin, w, h, label, score = rect
+                        json_item = {
+                            'xmin': xmin,
+                            'ymin': ymin,
+                            'w': w,
+                            'h': h,
+                            'label': label,
+                            'score': score,
+                        }
+                        json_items.append(json_item)
                     json_items_str = json.dumps(json_items)
                     redis_client.rpush(camera_data_queue_name, json_items_str)
                     f.write(json_items_str + '\n')
